3aca11a24dc738881793c640257c8a731532857f
belaeat <omyhossain@gmail.com>
2023-06-09 12:19:42 +0600

    footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,25 @@
 const Footer = () => {
     return (
         <div>
-            
+            <footer className="footer text-white p-10 bg-[#7cc051]">
+                <div>
+                    <h3 className="font-bold text-3xl">Saucy Culinary</h3>
+                    <p>Our Cooking School<br />Creating best chefs since 1950</p>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+
         </div>
     );
 };
