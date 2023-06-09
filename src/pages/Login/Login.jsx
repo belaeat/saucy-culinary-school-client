faef2544f311b6cf452b52de31fd52a1397baa84
belaeat <omyhossain@gmail.com>
2023-06-10 01:57:37 +0600

    firebase setup done with environment variable

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -17,8 +17,10 @@ const Login = () => {
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
                     </div>
+
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -26,14 +28,18 @@ const Login = () => {
                                 <input type="email" {...register("email", { required: true })} placeholder="Your email" className="input input-bordered" />
                                 {errors.email && <span className="text-red-500">This field is required</span>}
                             </div>
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="password" {...register("password", { required: true })} placeholder="Password" className="input input-bordered" />
+                                {errors.password && <span className="text-red-500">This field is required</span>}
+
                                 <label className="label">
                                     <p className="label-text-alt">New here? Please <Link className="text-[#7cc051]" to='/register'>Register.</Link></p>
                                 </label>
+                                
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn bg-[#7cc051] text-white">Login</button>
