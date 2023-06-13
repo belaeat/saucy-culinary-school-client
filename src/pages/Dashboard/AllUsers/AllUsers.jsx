e0ac8fab8fe3133b9c4bb16e5d08cd9122ea6bde
belaeat <omyhossain@gmail.com>
2023-06-14 02:12:28 +0600

    got jwt token

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery(['users'], async () => {
@@ -6,9 +7,65 @@ const AllUsers = () => {
         return res.json()
     })
 
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    refetch()
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is now an admin!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    }
+
     return (
         <div>
-            <h3 className='text-3xl font-semibold'>Total Users: {users.length}</h3>
+            <h3 className='text-3xl font-semibold my-8'>Total Users: {users.length}</h3>
+            {/* Table */}
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead className="bg-[#7cc051] text-white">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Make Instructor</th>
+                            <th>Make Admin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr
+                                key={user._id}
+                            >
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    {
+                                        user.role === 'admin' ? 'admin' : <button className="btn bg-[#fd8250] btn-xs text-white">Make Instructor</button>
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn bg-[#fd8250] btn-xs text-white">Make Admin</button>
+                                    }
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
