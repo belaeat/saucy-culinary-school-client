86cde18fc28c5a873e30243602f6b717900ff98b
belaeat <omyhossain@gmail.com>
2025-03-01 02:52:11 +0200

    update: readme

--- README.md
+++ README.md
@@ -1,30 +1,28 @@
-# Project Name: Saucy Culinary School | A Summar Camp School
+# Project Name: Saucy Culinary School - A Summer Camp School
 
 ## Features:
 
-* **Single Page Application.**  
-* **This website has a unique dashboard for admin, instructor and students.**
-* **It also have payment system. Anyone can make payment with their Card.**  
-* **Students can enroll to a class online.**  
-* **REST API.**
+- **Single Page Application.**
+- **This website has a unique dashboard for admin, instructor and students.**
+- **It also have payment system. Anyone can make payment with their Card.**
+- **Students can enroll to a class online.**
+- **REST API.**
 
 ## Packages Used:
 
-* **React.js Framework.**  
-* **Express.js.**
-* **MongoDB.**  
-* **React-hook-form.**  
-* **React-helmet.**
-* **Swiper-js.**
-* **React-router.**
-* **Sweet Alert 2.**
-* **React-tooltip.**
-* **TanStack-query.**
-* **React-icons.**
-* **Axios.**
-* **React-darkreaders.**
-
+- **React.js Framework.**
+- **Express.js.**
+- **MongoDB.**
+- **React-hook-form.**
+- **React-helmet.**
+- **Swiper-js.**
+- **React-router.**
+- **Sweet Alert 2.**
+- **React-tooltip.**
+- **TanStack-query.**
+- **React-icons.**
+- **Axios.**
 
 ## Links
 
-Live Website Link:  **[Live Website](https://saucy-culinary-school.web.app/)**
+Live Website Link: **[Live Website](https://saucy-culinary-school.netlify.app/)**
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,4 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
@@ -18,57 +16,68 @@ import AdminRoute from "./AdmitRoute";
 import ManageClasses from "../pages/Dashboard/ManageClasses/ManageClasses";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
         path: "/",
-        element: <Main></Main>,
-        errorElement: <ErrorPage></ErrorPage>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: 'login',
-                element: <Login></Login>
-            },
-            {
-                path: 'register',
-                element: <Register></Register>
-            },
-            {
-                path: 'instructors',
-                element: <Instructors></Instructors>
-            },
-            {
-                path: 'classes',
-                element: <Classes></Classes>
-            },
-        ],
-    },
-    {
-        path: 'dashboard',
-        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
-        children: [
-            {
-                path: 'mycart',
-                element: <MyCart></MyCart>
-            },
-            {
-                path: 'enrolled'
-            },
-            {
-                path: 'allusers',
-                element: <AllUsers></AllUsers>
-            },
-            {
-                path: 'addClass',
-                element: <InstructorRoute><AddClass></AddClass></InstructorRoute>
-            },
-            {
-                path: 'manageClasses',
-                element: <AdminRoute><ManageClasses></ManageClasses></AdminRoute>
-            }
-
-        ]
-    },
+        element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+      {
+        path: "instructors",
+        element: <Instructors></Instructors>,
+      },
+      {
+        path: "classes",
+        element: <Classes></Classes>,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard></Dashboard>
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "mycart",
+        element: <MyCart></MyCart>,
+      },
+      {
+        path: "enrolled",
+      },
+      {
+        path: "allusers",
+        element: <AllUsers></AllUsers>,
+      },
+      {
+        path: "addClass",
+        element: (
+          <InstructorRoute>
+            <AddClass></AddClass>
+          </InstructorRoute>
+        ),
+      },
+      {
+        path: "manageClasses",
+        element: (
+          <AdminRoute>
+            <ManageClasses></ManageClasses>
+          </AdminRoute>
+        ),
+      },
+    ],
+  },
 ]);
